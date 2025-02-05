b5d59964bcc3878604f2dfea014d85aabbba57af
hejixian <hejixian@bytedance.com>
2025-02-05 21:51:05 +0800

    输入输出练习

---
+++ CSharpLearning/C#入门/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharp入门
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise 1");
+            Console.WriteLine("请输入用户名、年龄、班级");
+            Console.ReadLine();
+
+
+            Console.WriteLine("Exercise 2");
+            Console.WriteLine("你喜欢什么运动啊？");
+            Console.ReadLine();
+            Console.WriteLine("好巧啊，我也喜欢这个运动");
+
+
+            Console.WriteLine("Exercise 3");
+            Console.WriteLine("**********");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("**********");
+
+        }
+    }
+}
